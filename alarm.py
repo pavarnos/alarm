d6688ebbcdf75a5dc21095c59cf9afed4cfb81cb
Steve Pavarno <pavarnos@gmail.com>
2015-12-31 11:59:55 +1300

    pushover config

--- alarm.py
+++ alarm.py
@@ -1,9 +1,16 @@
 import time
 import RPi.GPIO as io
 import requests;
+import ConfigParser
 
 #!/usr/bin/python
 print("Alarm monitor started")
+
+# load config
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'config.txt'))
+pushoverUser = config.get('pushover', 'user')
+pushoverApplication = config.get('pushover','application')
 
 # GPIO configuration
 io.setmode(io.BCM) 
@@ -12,16 +19,10 @@ window_pin = 24
 io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
 io.setup(window_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
 
-# ifttt configuration
-iftttBaseUrl = 'https://maker.ifttt.com/trigger/'
-iftttToken = '/with/key/dja7APuAMVvDhA1UZTway-'
-doorOpenUrl = iftttBaseUrl + 'DoorOpened' + iftttToken
-windowOpenUrl = iftttBaseUrl + 'WindowOpened' + iftttToken
-
 # pushover configuration
-def notifyPushover(eventName):
+def notifyPushover(eventName,pushoverApplication,pushoverUser):
     message = 'Office ' + eventName
-    response = requests.post('https://api.pushover.net/1/messages.json',data={'token':'a69mmvyRPHWqa1FLUT35xKYmhtx5QA','user':'uiHkY7LC2bw26QMdMByYZnYHr2ZEfS','message':message,'title':'Office Alarm'})
+    response = requests.post('https://api.pushover.net/1/messages.json',data={'token':pushoverApplication,'user':pushoverUser,'message':message,'title':'Office Alarm'})
     print(response)
 
 # assume the door is open so we don't get a false alarm on startup
@@ -34,7 +35,7 @@ while True:
     if io.input(door_pin):
         if not doorIsOpen:
             print("Door Opened")
-            notifyPushover("DoorOpened")
+            notifyPushover("DoorOpened",pushoverApplication,pushoverUser)
         doorIsOpen = True
     else:
         doorIsOpen = False
@@ -43,7 +44,7 @@ while True:
     if io.input(window_pin):
         if not windowIsOpen:
             print("Window Opened")
-            notifyPushover("WindowOpened")
+            notifyPushover("WindowOpened",pushoverApplication,pushoverUser)
         windowIsOpen = True
     else:
         windowIsOpen = False
